882be0f4450ef90d1c7dce1dcaa94d76c4ce0ad3
caixiaofan1 <378467584@qq.com>
2016-04-23 17:53:01 +0800

    Initial Commit

---
+++ wangyiNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wangyiNews
+//
+//  Created by 蔡晓凡 on 16/4/23.
+//  Copyright © 2016年 蔡晓凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wangyiNews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wangyiNews
+//
+//  Created by 蔡晓凡 on 16/4/23.
+//  Copyright © 2016年 蔡晓凡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
